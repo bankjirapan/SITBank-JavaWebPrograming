b67d73049bce560cff37b271bfd4c40fee388f59
9banker <jirapan_yankhan@hotmail.com>
2018-09-23 22:53:20 +0700

    [ EDIT ] withdrew finish

--- src/java/sitbank/controller/WithdrewServlet.java
+++ src/java/sitbank/controller/WithdrewServlet.java
@@ -7,16 +7,36 @@ package sitbank.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.Integer.parseInt;
+import java.util.Date;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+import sitbank.jpa.controller.AccountJpaController;
+import sitbank.jpa.controller.HistoryJpaController;
+import sitbank.jpa.controller.exceptions.RollbackFailureException;
+import sitbank.jpa.models.Account;
+import sitbank.jpa.models.History;
 
 /**
  *
  * @author bankcom
  */
 public class WithdrewServlet extends HttpServlet {
+
+    
+    
+    @PersistenceUnit(unitName = "SITBankPU")
+    EntityManagerFactory emf;
+
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,19 +49,66 @@ public class WithdrewServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet WithdrewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet WithdrewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+      
+        
+        HttpSession session = request.getSession(false);
+        Account AccountLoggedIn = (Account) session.getAttribute("LoggedIn");
+
+        if (request.getParameter("WithDrewAm") != null) {
+
+            int AmountWithdrew = parseInt(request.getParameter("WithDrewAm"));
+
+            AccountJpaController accountCtrl = new AccountJpaController(utx, emf);
+
+            Account withdrewAccount = accountCtrl.findAccount(AccountLoggedIn.getAccountid());
+
+            if (withdrewAccount != null) {
+
+                withdrewAccount.setBalance(withdrewAccount.getBalance() - AmountWithdrew);
+
+                try {
+
+                    accountCtrl.edit(withdrewAccount);
+
+                } catch (RollbackFailureException rbf) {
+                    System.out.println(rbf);
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+
+                //Add เข้าสู่ History
+                HistoryJpaController hisCtrl = new HistoryJpaController(utx, emf);
+
+                History his = new History(hisCtrl.getHistoryCount() + 1);
+
+                his.setTime(new Date());
+                his.setAmount(AmountWithdrew);
+                his.setBalance(withdrewAccount.getBalance() - AmountWithdrew);
+                his.setAccountid(AccountLoggedIn);
+                his.setMethod("WirhDrew");
+
+                try {
+
+                    hisCtrl.create(his);
+
+                } catch (RollbackFailureException rbf) {
+                    System.out.println(rbf);
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+
+                session.setAttribute("finish", "Withdrew Complate");
+                session.setAttribute("LoggedIn", withdrewAccount);
+                response.sendRedirect("MyAccount");
+                return;
+
+            }
+
         }
+
+        getServletContext().getRequestDispatcher("/WithdrewView.jsp").forward(request, response);
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
