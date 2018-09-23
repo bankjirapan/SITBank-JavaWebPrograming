ec226d6388b44e4f0ca20c688e9e4fe1b62ac7a7
9banker <jirapan_yankhan@hotmail.com>
2018-09-23 22:55:19 +0700

    [ EDIT ] Logout and clear session

--- src/java/sitbank/controller/LogoutServlet.java
+++ src/java/sitbank/controller/LogoutServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import sitbank.jpa.models.Account;
 
 /**
  *
@@ -29,19 +31,16 @@ public class LogoutServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LogoutServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LogoutServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+       
+          HttpSession session = request.getSession(false);
+        Account AccountLoggedIn = (Account) session.getAttribute("LoggedIn");
+        
+        if(session != null){
+            session.invalidate();
+            response.sendRedirect("Login");
+            return;
         }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
